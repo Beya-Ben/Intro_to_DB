66d781e8b6b10f2a3a48c8046174b7a85f583519
Beya <beya.ben2308@gmail.com>
2024-08-04 23:57:41 +0800

    Update task_2.sql again

--- task_2.sql
+++ task_2.sql
@@ -16,10 +16,10 @@ CREATE TABLE IF NOT EXISTS "Books" (
 );
 
 CREATE TABLE IF NOT EXISTS "Customers" (
-    "customer_id" INT AUTO_INCREMENT PRIMARY KEY,
-    "customer_name" VARCHAR(215) NOT NULL,
-    "email" VARCHAR(215) NOT NULL,
-    "address" TEXT NOT NULL
+    "customer_id " INT AUTO_INCREMENT PRIMARY KEY,
+    "customer_name VARCHAR(215)" NOT NULL,
+   "email VARCHAR(215)" NOT NULL,
+   "address TEXT" NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "Orders" (
